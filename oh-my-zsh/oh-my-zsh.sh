7bea54321056cc724dfd9ecea49734234c035f8c
hjkatz <hjkatz03@gmail.com>
2014-06-06 11:51:46 -0400

    added oh-my-zsh slim

---
+++ oh-my-zsh/oh-my-zsh.sh
@@ -0,0 +1,56 @@
+# Initializes Oh My Zsh
+
+# add a function path
+fpath=($ZSH/functions $ZSH/completions $fpath)
+
+# Load all of the config files in ~/oh-my-zsh that end in .zsh
+# TIP: Add files you don't want in git to .gitignore
+for config_file ($ZSH/lib/*.zsh); do
+  source $config_file
+done
+
+is_plugin() {
+  local base_dir=$1
+  local name=$2
+  test -f $base_dir/plugins/$name/$name.plugin.zsh \
+    || test -f $base_dir/plugins/$name/_$name
+}
+# Add all defined plugins to fpath. This must be done
+# before running compinit.
+for plugin ($plugins); do
+    fpath=($ZSH/plugins/$plugin $fpath)
+done
+
+# Figure out the SHORT hostname
+if [ -n "$commands[scutil]" ]; then
+  # OS X
+  SHORT_HOST=$(scutil --get ComputerName)
+else
+  SHORT_HOST=${HOST/.*/}
+fi
+
+# Save the location of the current completion dump file.
+ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
+
+# Load and run compinit
+autoload -U compinit
+compinit -i -d "${ZSH_COMPDUMP}"
+
+# Load all of the plugins that were defined in ~/.zshrc
+for plugin ($plugins); do
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+done
+
+# Load the theme
+if [ "$ZSH_THEME" = "random" ]; then
+  themes=($ZSH/themes/*zsh-theme)
+  N=${#themes[@]}
+  ((N=(RANDOM%N)+1))
+  RANDOM_THEME=${themes[$N]}
+  source "$RANDOM_THEME"
+  echo "[oh-my-zsh] Random theme '$RANDOM_THEME' loaded..."
+else
+  if [ ! "$ZSH_THEME" = ""  ]; then
+      source "$ZSH/themes/$ZSH_THEME.zsh-theme"
+  fi
+fi
