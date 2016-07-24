1218e534a29f70dec9e160afc0ca2bf0ffe4be4d
Harrison Katz <harrison@neadwerx.com>
2016-07-24 15:42:40 -0400

    updated sudo and websearch plugins

--- zshrc.plugins/sudo/sudo.plugin.zsh
+++ zshrc.plugins/sudo/sudo.plugin.zsh
@@ -13,10 +13,11 @@
 # ------------------------------------------------------------------------------
 
 sudo-command-line() {
-[[ -z $BUFFER ]] && zle up-history
-[[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
-zle end-of-line 
+    [[ -z $BUFFER ]] && zle up-history
+    [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
+    zle end-of-line 
 }
 zle -N sudo-command-line
+
 # Defined shortcut keys: [Esc] [Esc]
 bindkey "\e\e" sudo-command-line
