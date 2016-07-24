44ffcd46af88a670e7e0d5494ae56c8d185a0cf2
Harrison Katz <harrison@neadwerx.com>
2016-07-23 23:42:23 -0400

    reviewed, commented, and updated setopts

--- zshrc.lib/setopts.zsh
+++ zshrc.lib/setopts.zsh
@@ -1,50 +1,47 @@
 # Zsh settings (see: http://zsh.sourceforge.net/Doc/Release/Options.html)
 
-# TODO: pull these out of oh-my-zsh
-
 # in order on the options page
 
 # Changing directories
-setopt AUTO_CD
-setopt AUTO_PUSHD
-setopt PUSHD_IGNORE_DUPS
-setopt PUSHDMINUS
+setopt AUTO_CD           # auto cd to directories without needing to type cd
+setopt AUTO_PUSHD        # auto push directories onto the directory stack
+setopt CDABLE_VARS       # if the cd command doesn't exist, try ~/command
+setopt PUSHD_IGNORE_DUPS # don't push duplicates onto the directory stack
+setopt PUSHDMINUS        # cd -3 works as expected
 
 # Completion
-unsetopt MENU_COMPLETE   # do not autoselect the first completion entry
-unsetopt FLOWCONTROL
-setopt AUTO_MENU         # show completion menu on succesive tab press
-setopt COMPLETE_IN_WORD
-setopt ALWAYS_TO_END
+setopt   ALWAYS_TO_END    # move cursor to the end of the completion if a match is found
+setopt   AUTO_MENU        # show completion menu on succesive tab press
+setopt   COMPLETE_IN_WORD # keep cursor inside the word while completion is happening
+setopt   GLOB_COMPLETE    # complete globs to a pattern match instead of an insertion
+unsetopt MENU_COMPLETE    # do not autoselect the first completion entry
 
-# Correction
-setopt CORRECT
+# Expansion and Globbing
+setopt EXTENDED_GLOB # turn on extended globbing :)
+setopt MARK_DIRS     # append a '/' to all matched directories
 
 # History
-setopt APPEND_HISTORY
-setopt EXTENDED_HISTORY
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_IGNORE_DUPS # ignore duplication command history list
-setopt HIST_IGNORE_SPACE
-setopt HIST_VERIFY
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY # share command history data
+setopt APPEND_HISTORY         # every zsh session appends to the histfile instead of overwriting
+setopt EXTENDED_HISTORY       # save extra information in the histfile ‘: <beginning time>:<elapsed seconds>;<command>’
+setopt HIST_EXPIRE_DUPS_FIRST # if we must delete from the history, delete duplicates first
+setopt HIST_IGNORE_DUPS       # ignore duplication command history list
+setopt HIST_IGNORE_SPACE      # commands that start with a space do not get recorded to the history
+setopt HIST_VERIFY            # don’t execute lines with history expansion, instead put them on the current line expanded
+setopt INC_APPEND_HISTORY     # write to the histfile immediately on command execution
+setopt SHARE_HISTORY          # share command history data between zsh processes
 
-# Jobs
-setopt LONG_LIST_JOBS
+# Input/Output
+setopt   CORRECT      # correct the spelling of commands
+setopt   DVORAK       # correct for spelling mistakes with Dvorak layout
+unsetopt FLOWCONTROL  # output flow control via start/stop characters is disabled
+setopt   RM_STAR_WAIT # safety first for rm * or rm path/*
 
-# Comments
-setopt INTERACTIVE_COMMENTS
+# Job Control
+setopt AUTO_CONTINUE  # when `disown`-ing a job a SIGCONT is automatically sent to the process to start it up
+setopt LONG_LIST_JOBS # list jobs in the long format by default
 
-# Something?
-setopt MULTIOS
-setopt PROMPT_SUBST
+# Prompting
+setopt PROMPT_SUBST # make expansion and substitution work inside prompts
 
-setopt APPEND_HISTORY
-setopt SHARE_HISTORY
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_NO_STORE
-setopt RM_STAR_WAIT
-setopt EXTENDED_GLOB
-setopt DVORAK
+# Scripts and Functions
+setopt MULTIOS # perform implicit `tee`s or `cat`s when multiple redirections are attempted
