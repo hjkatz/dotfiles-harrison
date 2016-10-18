9b22ae0a42a40f969a103e6db7af259491527138
Harrison Katz <harrison@neadwerx.com>
2016-10-18 08:44:00 -0400

    added ~/.local/bin to path

--- zshrc.lib/environment.zsh
+++ zshrc.lib/environment.zsh
@@ -1,7 +1,7 @@
 # ENV exports
 
 # Set Path
-export PATH=$PATH:/usr/local/git/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$DOTFILES/bin
+export PATH=$PATH:/usr/local/git/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$DOTFILES/bin:$HOME/.local/bin
 
 # Set default editor
 export EDITOR=/usr/bin/vim
