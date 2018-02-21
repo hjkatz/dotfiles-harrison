8957b2582bdf3101f815646d00c6b7580a1571ff
Harrison Katz <hkatz@squarespace.com>
2018-02-21 15:55:46 -0500

    Update completions some more or something

--- zshrc.lib/globals.zsh
+++ zshrc.lib/globals.zsh
@@ -32,7 +32,7 @@ export GLOBALS__DOTFILES_TEMPLATES_PATH="$DOTFILES/templates"
 export GLOBALS__DOTFILES_COMPILED_PATH="$DOTFILES/compiled"
 
 # custom / vendor completions path
-export GLOBALS__DOTFILES_COMPLETIONS_PATH="$DOTFILES/.completions"
+export GLOBALS__DOTFILES_COMPLETIONS_PATH="$DOTFILES/completions"
 
 # exit task hook function name for zshrc_local
 export GLOBALS__DOTFILES_EXIT_HOOK_FUNCTION="_local_exit_function"
