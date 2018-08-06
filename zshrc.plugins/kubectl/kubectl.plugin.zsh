4e1e9563e0256f9ab5c54ff53109d8d14ab527cb
Harrison Katz <hkatz@squarespace.com>
2018-08-06 15:24:45 -0400

    Transition to kube-ps1

--- zshrc.plugins/kubectl/kubectl.plugin.zsh
+++ zshrc.plugins/kubectl/kubectl.plugin.zsh
@@ -11,77 +11,15 @@ alias -g kall="all,configmaps,deployments.apps,servicemonitors,secrets"
 compdef kb=kubectl
 compdef kube=kubectl
 
-function kctx () {
-    # no args, print contexts
-    if [[ "$#" == 0 ]] ; then
-        kubectl config get-contexts
-    else
-        kubectl config use-context "$@"
-    fi
-}
-
-#### PROMPT ####
-# configure:
-#   - ZSH_THEME_KUBECTL_PREFIX            : prefix for prompt
-#   - ZSH_THEME_KUBECTL_SUFFIX            : suffix for prompt
-#   - ZSH_THEME_KUBECTL_SEPARATOR         : separator between context and namespace
-#   - ZSH_THEME_KUBECTL_SHOW_NAMESPACE    : show the namespace or note
-#   - ZSH_THEME_KUBECTL_DEFAULT_NAMESPACE : what the default namespace should be (if empty, then no separator will be shown)
-#   - ZSH_THEME_KUBECTL_KUBECONFIG        : path to your kube config
-
-# defaults
-ZSH_THEME_KUBECTL_PREFIX="{"
-ZSH_THEME_KUBECTL_SUFFIX="}"
-ZSH_THEME_KUBECTL_SEPARATOR="/"
-ZSH_THEME_KUBECTL_SHOW_NAMESPACE=true
-ZSH_THEME_KUBECTL_DEFAULT_NAMESPACE=""
-ZSH_THEME_KUBECTL_KUBECONFIG="$HOME/.kube/config"
+# kubectx config
+export KUBECTX_CURRENT_FGCOLOR=$(tput setaf 6) # blue text
+export KUBECTX_CURRENT_BGCOLOR=$(tput setaf 7) # white background
 
 # show the prompt information
 function kubectl_prompt_info () {
     [[ $GLOBALS__SHOW_PROMPT_HASH[kubectl] != true ]] && return
 
-    _get_kubectl_prompt
-    echo "${ZSH_THEME_KUBECTL_PREFIX}${ZSH_KUBECTL_PROMPT}${ZSH_THEME_KUBECTL_SUFFIX}"
-}
-
-# gather info and build the kubectl prompt var
-function _get_kubectl_prompt() {
-    # try out the provided kubeconfig or default to the environment
-    kubeconfig="$ZSH_THEME_KUBECTL_KUBECONFIG"
-    if [[ -n "$KUBECONFIG" ]]; then
-        kubeconfig="$KUBECONFIG"
-    fi
-
-    if ! [[ -r $kubeconfig ]] ; then
-        ZSH_KUBECTL_PROMPT="kubeconfig missing!!"
-        return 1
-    fi
-
-    if ! context="$(kubectl config current-context 2>/dev/null)" ; then
-        ZSH_KUBECTL_PROMPT="current-context is not set!!"
-        return 1
-    fi
-
-    if [[ $ZSH_THEME_KUBECTL_SHOW_NAMESPACE != true ]] ; then
-        # empty namespace if we shouldn't show one
-        namespace=""
-    else
-        namespace="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.namespace}")"
-
-        if [[ -z $namespace ]] ; then
-            namespace="$ZSH_THEME_KUBECTL_DEFAULT_NAMESPACE"
-        fi
-    fi
-
-    # if we have a namespace, show it and the separator
-    if [[ -n $namespace ]] ; then
-        ZSH_KUBECTL_PROMPT="${context}${ZSH_THEME_KUBECTL_SEPARATOR}${namespace}"
-    else # don't
-        ZSH_KUBECTL_PROMPT="${context}"
-    fi
-
-    return 0
+    echo "$(kube_ps1)"
 }
 
 function kb_get_all () {
