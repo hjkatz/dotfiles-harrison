27acf98b6c618a1ab33f1c1633e2d1b4fa693a9d
Harrison Katz <harrison.katz@gemini.com>
2022-05-10 16:12:42 -0400

    Add gpg git config settings

--- zshrc.lib/user.zsh
+++ zshrc.lib/user.zsh
@@ -2,9 +2,11 @@
 
 # default user information (for gitconfig, mainly)
 # local configs are found in ~/.zshrc_local
-GIT_NAME="Harrison Katz"
-GIT_EMAIL="hjatz03@gmail.com"
-GIT_CODE_DIR="$HOME/Code"
+export GIT_NAME="Harrison Katz"
+export GIT_EMAIL="hjatz03@gmail.com"
+export GIT_GPG_SIGNINGKEY="" # set in ~/.zshrc_local
+export GIT_GPG_SIGN="false" # don't sign git actions by default
+export GIT_CODE_DIR="$HOME/Code"
 
 # how long in seconds to wait between update checks
 UPDATE_CHECK_INTERVAL_SECONDS=86400
