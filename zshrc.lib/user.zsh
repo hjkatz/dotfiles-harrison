76ce153d2e3dcc3f4ea775c6072e0a8e2b0423f8
Harrison Katz <harrison.katz@gemini.com>
2022-05-11 18:02:09 -0400

    Add GIT_ORG config to gitconfig

--- zshrc.lib/user.zsh
+++ zshrc.lib/user.zsh
@@ -4,6 +4,7 @@
 # local configs are found in ~/.zshrc_local
 export GIT_NAME="Harrison Katz"
 export GIT_EMAIL="hjatz03@gmail.com"
+export GIT_ORG="hjkatz"
 export GIT_GPG_SIGNINGKEY="" # set in ~/.zshrc_local
 export GIT_GPG_SIGN="false" # don't sign git actions by default
 export GIT_CODE_DIR="$HOME/Code"
