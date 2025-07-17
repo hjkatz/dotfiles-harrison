1d145e02c40a37699f47157cda1e9116163146f1
Harrison Katz <harrison.katz@duolingo.com>
2025-07-17 13:44:04 -0400

    Add some uv helper functions

--- zshrc.plugins/virtualenv/virtualenv.plugin.zsh
+++ zshrc.plugins/virtualenv/virtualenv.plugin.zsh
@@ -16,8 +16,83 @@ function virtualenv_prompt_info () {
 # disables prompt mangling in virtual_env/bin/activate
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
+# stores the currently active virtual environment root
+export _CURRENT_VENV_ROOT=""
+auto_venv () {
+    if [[ -n $VIRTUAL_ENV ]]; then
+        # we are in a virtualenv, so set the current venv root
+        _CURRENT_VENV_ROOT=$(dirname "$VIRTUAL_ENV")
+    else
+        # no virtualenv, reset the current venv root
+        _CURRENT_VENV_ROOT=""
+    fi
+
+    if [[ -e .venv/ ]]; then
+        reactivate # from virtualenv.plugin.zsh
+    elif [[ -n $VIRTUAL_ENV ]] && [[ `pwd` != "$_CURRENT_VENV_ROOT"* ]]; then
+        # left the venv dir
+        deactivate
+    fi
+}
+
+# add `setuv` to be called automatically anytime we change directories
+autoload -Uz add-zsh-hook
+add-zsh-hook -Uz chpwd auto_venv
+
 # reactivate the virtualenv
 function reactivate () {
     [[ -n $VIRTUAL_ENV ]] && deactivate
     [[ -e ".venv/" ]] && source ".venv/bin/activate"
+}
+
+# uv helper functions
+
+function uv_activate () {
+    reactivate
+}
+
+function uv_deactivate () {
+    deactivate
+}
+
+function uv_create_venv () {
+    uv venv "$@"
+}
+
+function uv_compile () {
+    if [[ -n $VIRTUAL_ENV ]]; then
+        uv pip compile -o requirements.txt requirements.in --emit-index-url
+    else
+        echo "No active venv found (run reactivate?)"
+        return 1
+    fi
+}
+
+function uv_install () {
+    if [[ -n $VIRTUAL_ENV ]]; then
+        uv pip install -r requirements.txt
+    else
+        echo "No active venv found (run reactivate?)"
+        return 1
+    fi
+}
+
+function uv_new () {
+    uv_create_venv "$@"
+    uv_activate
+    uv_compile
+    uv_install
+}
+
+function uv_delete () {
+    if [[ -n $VIRTUAL_ENV ]]; then
+        deactivate
+    fi
+
+    if [[ -d ".venv" ]]; then
+        rm -rf ".venv"
+    else
+        echo "No venv found to delete."
+        return 1
+    fi
 }
